ac9e892a86e17afdef912caa4180298defc11e07
KanishkNoir <kanishksingh.1106@gmail.com>
2023-07-05 16:33:17 +0530

    first commit database connected

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const routes = require('./routes')
+const cors = require('cors')
+const router = require('./routes')
+
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+const db = mongoose.connection
+db.on('error', (error)=> console.error(error))
+db.once('open', ()=> console.log('Connected to Databse'))
+
+app.listen(process.env.PORT, () =>{
+    console.log("Fibr Quiz API is running!")
+})
