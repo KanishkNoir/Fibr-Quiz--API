27109844e76a7da911606edbb066dee9764837c4
KanishkNoir <kanishksingh.1106@gmail.com>
2023-07-06 23:26:38 +0530

    get all questions||delete single question by id

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,64 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
 const Question = require('./models/Question')
 
-//creating a question
-router.post('/questions', async (req, res) => {
-    try {
-        const { description } = req.body
-        const { alternatives } = req.body
+//creating one question
 
-        const question = await Question.create({
-            description,
-            alternatives
+router.post('/questions', async(req, res) => {
+    const {description, alternatives} = req.body
+	try {
+		const newQuestion = await Question.create({description, alternatives});
+
+        newQuestion.save().then(() =>{
+            res.status(201).json({message: 'Question saves succesfully'})
         })
+	} catch (error){
+		return res.status(500).json({"error": error})
+	}
+})
 
-        return res.status(201).json(question)
-    } catch (error) {
-        return res.status(500).json({"error":error})
+// router.post('/questions', (req, res) => {
+//     const { description, alternatives } = req.body;
+  
+//     const newQuestion = new Question.create({
+//       description,
+//       alternatives,
+//     });
+  
+//     newQuestion.save()
+//       .then(() => {
+//         res.status(201).json({ message: 'Question saved successfully' });
+//       })
+//       .catch((error) => {
+//         res.status(500).json({ error: 'Error saving question' });
+//       });
+//   });
+
+//get all questions
+router.get('/questions', async (req,res)=>{
+    try{
+        const questions = await Question.find()
+        return res.status(200).json(questions)
+    }catch(error){
+        return res.status(500).json({"error": error})
     }
 })
 
 // deleting a question
-router.delete('/questions/:id', (req, res) =>{
-
+router.delete('/questions/:id', async (req, res) =>{
+    try{
+        const _id =req.params.id
+        const question =await Question.deleteOne({_id})
+    if(question.deletedCount===0){
+        return res.status(404).json()
+    }
+    else{
+        return res.status(204).json()
+    }
+    }catch(error){
+            return res.status(500).json({"error": error})
+        }
 })
 
 //test
